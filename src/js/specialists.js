0dcfffa014df8a13d16fbc33e2f0b6e698d5ed27
mazax1ct <vova@giveandget.ru>
2021-04-05 15:53:11 +0300

    all blocks + optim img

---
+++ src/js/specialists.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  if($('.js-specialists-slider').length) {
+    $('.js-specialists-slider').slick({
+      mobileFirst: true,
+      dots: false,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      prevArrow: '<button class="slick-arrow slick-prev" aria-label="Назад" type="button"><svg class="slick-arrow__arrow" aria-hidden="true"><use xlink:href="#arrow_left"/></svg></button>',
+      nextArrow: '<button class="slick-arrow slick-next" aria-label="Вперед" type="button"><svg class="slick-arrow__arrow" aria-hidden="true"><use xlink:href="#arrow_right"/></svg></button>',
+      appendArrows: $('.specialists__arrows')
+    });
+  }
+});
